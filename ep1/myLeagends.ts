51283f21826a0f3880715fe88e94f7c7053f57d6
phurinintradu <intraklaza@gmail.com>
2024-07-19 11:44:50 +0700

    กล้า

---
+++ ep1/myLeagends.ts
@@ -0,0 +1,10 @@
+const nickname: sring = 'kla'
+const fristname: string ="phurin"
+
+let x: number = 1
+let y: boolean =true
+let z:string ='1'
+let v: number =2
+console.log(x===z);
+console.log(x<=v)
+console.log(x)

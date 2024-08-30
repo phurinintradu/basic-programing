d15cbf4e0c92b15feb37d6aa1f8c1aa15b7beff4
phurinintradu <intraklaza@gmail.com>
2024-08-30 09:06:46 +0700

    ep6

---
+++ ep6/index.ts
@@ -0,0 +1,9 @@
+const score: number[] =[10,23,26,30,33]
+let sum: number=0
+for (let i = 0 ; i < score. length; i++) {
+     sum = score[i] + score[i+i]  
+}
+ const avg = sum /score.length
+
+ console.log('Total: ', sum)
+ console.log('avg: ',avg)
